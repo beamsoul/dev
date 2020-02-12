6412c90542c10e8ceaeae0329d7bde6917b97d22
beamsoul <50894237+beamsoul@users.noreply.github.com>
2020-02-11 21:49:08 -0600

    pushing

--- linux/main.tf
+++ linux/main.tf
@@ -1,4 +1,9 @@
-resource "aws_instance" {
-  ami             = "ami-02eac2c0129f6376b"
+
+resource "aws_instance" "my-test-instance" {
+  ami             = "${data.aws_ami.centos.id}"
   instance_type   = "t2.micro"
 
+  tags {
+    Name = "test-instance"
+  }
+}
