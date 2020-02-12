35afdf83bf3bb3aff93bd8bc70faa1ecba7c8d87
beamsoul <50894237+beamsoul@users.noreply.github.com>
2020-02-11 21:38:14 -0600

    pushing

--- linux.tf
+++
@@ -1,9 +0,0 @@
-provider "aws" {
-    region = "us-east-1"
-}
-
-resource "aws_instance" "centos" {
-  ami = "ami-02eac2c0129f6376b"
-  instance_type = "t1.micro"
-  tags {
-    Name = "HelloWorld"
---
+++ linux/main.tf
@@ -0,0 +1,8 @@
+resource "aws_instance" "my-test-instance" {
+  ami             = "ami-02eac2c0129f6376b"
+  instance_type   = "t2.micro"
+
+  tags {
+    Name = "test-instance"
+  }
+}
